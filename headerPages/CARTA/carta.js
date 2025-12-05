33225e0d96d2cf38f3c5b06a6c42b5959bd98214
João <joaovictor.tsouzaz@gmail.com>
2025-12-05 16:47:49 -0300

    Vai corinthians

---
+++ headerPages/CARTA/carta.js
@@ -0,0 +1,5 @@
+    function mostrarMensagem() {
+        const msg = document.getElementById("mensagem");
+        msg.innerText = "Você é a melhor parte dos meus dias. Te amo! 💖";
+        msg.style.display = "block";
+    }
